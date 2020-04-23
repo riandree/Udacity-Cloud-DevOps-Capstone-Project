483f60d20cde9b5917aa382ab8ac2a5e1c504f03
rieck5 <rieck.andre@guj.de>
2020-04-23 16:11:46 +0200

    eks cluster

--- infrastructure/createInfrastructure.sh
+++ infrastructure/createInfrastructure.sh
@@ -2,11 +2,33 @@
 
 aws cloudformation create-stack \
        --stack-name todo-webapp \
-       --template-body file://infrastructure.yml
+       --template-body file://infrastructure.yml \
+       --capabilities CAPABILITY_NAMED_IAM
 #       --parameters file://parameters.json \
-#       --capabilities CAPABILITY_NAMED_IAM
 
 if [[ "$?" == 0 ]]; then
   echo "Waiting for stack creation to complete ..."
   aws cloudformation wait stack-create-complete 
+else 
+  exit 1
+fi
+
+echo "creating VPC"
+
+aws cloudformation create-stack --stack-name todo-webapp-vpc --template-body file://amazon-eks-vpc-private-subnets.yaml
+
+if [[ "$?" == 0 ]]; then
+  echo "Waiting for stack creation to complete ..."
+  aws cloudformation wait stack-create-complete 
+else 
+  exit 1
+fi
+
+aws cloudformation create-stack --stack-name todo-webapp-eks --template-body file://amazon-eks-cluster.yaml
+
+if [[ "$?" == 0 ]]; then
+  echo "Waiting for stack creation to complete ..."
+  aws cloudformation wait stack-create-complete 
+else 
+  exit 1
 fi
---
+++ infrastructure/updateInfrastructure.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+aws cloudformation update-stack \
+       --stack-name todo-webapp \
+       --template-body file://infrastructure.yml \
+       --capabilities CAPABILITY_NAMED_IAM
+#       --parameters file://parameters.json \
+
+if [[ "$?" == 0 ]]; then
+  echo "Waiting for stack update to complete ..."
+  aws cloudformation wait stack-update-complete 
+else
+  exit 1
+fi
+
+echo "updating VPC"
+
+aws cloudformation update-stack --stack-name todo-webapp-vpc --template-body file://amazon-eks-vpc-private-subnets.yaml
+
+if [[ "$?" == 0 ]]; then
+  echo "Waiting for stack update to complete ..."
+  aws cloudformation wait stack-update-complete 
+else
+  exit 1
+fi
+
+aws cloudformation update-stack --stack-name todo-webapp-eks --template-body file://amazon-eks-cluster.yaml
+
+if [[ "$?" == 0 ]]; then
+  echo "Waiting for stack update to complete ..."
+  aws cloudformation wait stack-update-complete 
+else
+  exit 1
+fi
