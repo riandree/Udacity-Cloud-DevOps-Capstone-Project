462e064ef03031ba3cec3e4142bd2e95d06f4f5c
rieck5 <rieck.andre@guj.de>
2020-04-25 17:05:15 +0200

    send JWT with axios Requests when logged in

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,66 +1,70 @@
 <template>
-  <div id="app">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
+    <div id="app">
+        <div>
+            <p>
+                If Element is successfully added to this project, you'll see an
+                <code v-text="'<el-button>'"></code>
+                below
+            </p>
+            <el-button @click="handleClick">el-button</el-button>
+        </div>
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <amplify-authenticator></amplify-authenticator>
+        <amplify-sign-out v-if="isSignedIn" button-text="Log me out"></amplify-sign-out>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <amplify-authenticator></amplify-authenticator>
-  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import { components } from 'aws-amplify-vue'
+    import HelloWorld from './components/HelloWorld.vue';
+    import {AmplifyEventBus, components} from 'aws-amplify-vue';
+    import axios from 'axios';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-    ...components
-  },
-    data () {
-    return {
-      authConfig: {
-          usernameAttributes: 'user email',
-          signUpConfig: {
-            header: 'Sign Up',
-            hideAllDefaults: true,
-            defaultCountryCode: '1',
-            signUpFields: [
-              {
-                label: 'email',
-                key: 'email',
-                required: true,
-                displayOrder: 1,
-                type: 'string',
-              },
-              {
-                label: 'Password',
-                key: 'password',
-                required: true,
-                displayOrder: 2,
-                type: 'password'
-              }
-            ]
-          }
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld,
+            ...components
+        },
+        data() {
+            return {
+                isSignedIn: false
+            }
+        },
+        methods: {
+            determinLoginStatus: function () {
+                this.$Amplify.Auth.currentSession().then(session => {
+                    this.isSignedIn = true;
+                }).catch(() => {
+                    this.isSignedIn = false;
+                })
+            },
+            handleClick() {
+              console.log("fetching");
+              axios({
+                method: 'get',
+                url: '/api/v1/hello',
+                baseURL: 'http://localhost:8080'
+              }). then((result) => {
+                console.log(result);
+              })
+            }
+        },
+        created() {
+            this.determinLoginStatus();
+            AmplifyEventBus.$on('authState', () => {
+                this.determinLoginStatus();
+            });
         }
-      }
-    }
-}
+    };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,18 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './plugins/element.js';
+import axios from 'axios';
 
 import Amplify, * as AmplifyModules from 'aws-amplify'
-import { AmplifyPlugin } from 'aws-amplify-vue'
-//import awsconfig from './aws-exports'
-
-import { AmplifyEventBus } from 'aws-amplify-vue';
+import {AmplifyEventBus, AmplifyPlugin} from 'aws-amplify-vue'
 
 Amplify.configure({
-    Auth : {
+    Auth: {
         region: 'eu-central-1',
-        userPoolId: 'eu-central-1_Dw8ZNiETz',
-        userPoolWebClientId: '6h4ndn21ij45q3rqtrj2m0r6nv'
+        userPoolId: 'eu-central-1_wr3AourNH',
+        userPoolWebClientId: '6gi7km51i3jou67q1j5pdenavv'
     }
 });
 
@@ -21,41 +19,21 @@ Vue.use(AmplifyPlugin, AmplifyModules)
 
 Vue.config.productionTip = false;
 
-const vue=new Vue({ 
-  router,
-  render: h => h(App)
+const vue = new Vue({
+    router,
+    render: h => h(App)
 }).$mount('#app');
 
-
 AmplifyEventBus.$on('authState', info => {
-  console.log(`Here is the auth event that was just emitted by an Amplify component: ${info}`)
-  vue.$Amplify.Auth.currentSession().then(session => {
-    console.log(session);
-    // idToken.jwtToken
-
-    // Keys siehe : https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_Dw8ZNiETz/.well-known/jwks.json
-    // siehe auch : https://docs.aws.amazon.com/de_de/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html
-    //              https://aws.amazon.com/premiumsupport/knowledge-center/decode-verify-cognito-json-token/
-    //              https://github.com/rrohitramsen/aws_jwt_cognito
-    /*{
-      "sub": "deda321f-7ccb-4309-978a-8fbfb7882a4f",
-      "email_verified": true,
-      "iss": "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_Dw8ZNiETz",
-      "phone_number_verified": false,
-      "cognito:username": "andre.rieck",
-      "aud": "6h4ndn21ij45q3rqtrj2m0r6nv",
-      "event_id": "2fdb0875-bdb6-4cce-ae1f-a67255deacfd",
-      "token_use": "id",
-      "auth_time": 1586971923,
-      "phone_number": "+49040123456",
-      "exp": 1586975523,
-      "iat": 1586971923,
-      "email": "blubb@test.de"
-    }*/
-
-
-
-  });
+    console.log(`Here is the auth event that was just emitted by an Amplify component: ${info}`)
+    if (info === 'signedIn') {
+        vue.$Amplify.Auth.currentSession().then(session => {
+            console.log(session.idToken.jwtToken);
+            axios.defaults.headers.common['Authorization'] = `Bearer ${session.idToken.jwtToken}`;
+        })
+    } else if (info === 'signedOut') {
+        axios.defaults.headers.common['Authorization'] = undefined;
+    }
 });
 
 
