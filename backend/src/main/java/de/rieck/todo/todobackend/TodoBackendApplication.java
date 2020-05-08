7e798423320dfc201880af873aae426f1034ed90
rieck5 <rieck.andre@guj.de>
2020-05-08 14:41:19 +0200

    busy waiting at startup time so we can 'see' the deployment process

--- backend/src/main/java/de/rieck/todo/todobackend/TodoBackendApplication.java
+++ backend/src/main/java/de/rieck/todo/todobackend/TodoBackendApplication.java
@@ -3,11 +3,16 @@ package de.rieck.todo.todobackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class TodoBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoBackendApplication.class, args);
-	}
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("busy waiting 10 seconds");
+        TimeUnit.SECONDS.sleep(10); // Slow down a bit to so we are able to watch the deployment process
+        System.out.println("done busy waiting");
+    	SpringApplication.run(TodoBackendApplication.class, args);
+    }
 
 }
